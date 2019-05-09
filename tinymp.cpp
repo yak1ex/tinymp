a9ffabfb29007ac6e96c6b89be2471045828bf39
Yak! <yak1ex@users.noreply.github.com>
2019-05-09 12:47:55 +0900

    Re-order delcarations.

--- tinymp.cpp
+++ tinymp.cpp
@@ -10,239 +10,23 @@
 // distributed under the CC0-1.0
 class tinymp
 {
+private:
 	typedef unsigned int value_type;
 	typedef std::numeric_limits<value_type> limits_type;
 	typedef unsigned long long widen_type;
 	typedef std::numeric_limits<widen_type> wlimits_type;
 	typedef std::vector<value_type> vector_type;
 	typedef const std::vector<value_type> cvector_type;
-	vector_type v;
-	bool nonneg;
-	void normalize() {
-		while(v.size() > 1 && v.back() == 0) v.pop_back();
-		if(is_zero()) nonneg = true;
-	}
 
-	template<typename T> // T = vector<...>
-	class offseter // minimal offset vector access
-	{
-	public:
-		typedef typename T::value_type value_type;
-	private:
-		decltype(std::declval<T>().begin()) st;
-		std::size_t sz; // valid digits
-		std::size_t cap;
-		std::size_t off;
-		static value_type& zero() {
-			static value_type zero_;
-			return zero_;
-		}
-	public:
-		offseter(T& pv_, std::size_t offset_ = 0) : st(pv_.begin()), sz(pv_.size()), cap(pv_.capacity()), off(offset_) {}
-		template<typename U> // should limit U
-		offseter(const offseter<U>& o) : st(o.start()), sz(o.size() - o.offset()), cap(o.capacity()), off(o.offset()) {}
-#ifdef KARATSUBA
-		offseter(decltype(st) st_, decltype(st) end_, std::size_t sz_, std::size_t offset_) : st(st_), sz(sz_), cap(end_ - st_), off(offset_) {}
-#endif
-		class riterator { // lexicographical_compare() requires InputIterator
-			const offseter *p;
-			std::size_t pos; // [0, p->sz + p->offset]: (p->sz + p->off) points to p->st[p->sz - 1]
-		public:
-			typedef std::input_iterator_tag iterator_category;
-			typedef value_type value_type;
-			typedef std::ptrdiff_t difference_type;
-			typedef const value_type* pointer;
-			typedef const value_type& reference;
-			riterator(const offseter *p_, std::size_t pos_) : p(p_), pos(pos_) {}
-			reference operator*() { return pos >= p->off + 1 ? p->st[pos - p->off - 1] : zero(); }
-			riterator& operator++() { --pos; return *this; }
-			riterator operator++(int) { riterator r(*this); --pos; return r; /* NRVO */ }
-			bool operator==(const riterator& other) const { return p == other.p && pos == other.pos; }
-			bool operator!=(const riterator& other) const { return !(*this == other); }
-		};
-		const value_type& operator[](std::size_t idx) const { return idx >= off ? st[idx - off] : zero(); }
-		value_type& operator[](std::size_t idx) { return idx >= off ? st[idx - off] : zero(); }
-		riterator rbegin() const { return riterator(this, sz + off); }
-		riterator rend() const { return riterator(this, 0); }
-		std::size_t size() const { return sz + off; }
-		std::size_t offset() const { return off; }
-		std::size_t capacity() const { return cap; }
-		decltype(st) start() const { return st; }
-		void resize(std::size_t sz_) { if(sz_ <= cap + off && sz_ > off) { for(std::size_t idx = sz; idx < sz_ - off; ++idx) st[idx] = 0; sz = sz_ - off; } else throw std::bad_alloc(); }
-		void push_back(const value_type& t) { if(sz < cap) { st[sz] = t; ++sz; } else throw std::bad_alloc(); }
-#ifdef KARATSUBA
-		offseter offset_view(std::size_t o) const { return { st, st + sz, sz, off + o }; }
-		offseter sub_view(std::size_t s, std::size_t e) const { if(off != 0) throw std::logic_error("offset should be 0"); return { st + s, st + e, e - s, off }; }
-#endif
-		void dump(std::ostream &os) const {
-			os << "[sz:" << sz << ",cap:" << cap << ",off:" << off << ']';
-			for(std::size_t idx = 0; idx < sz; ++idx) os << ',' << st[idx];
-			os << std::endl;
-		}
-	};
-	typedef offseter<vector_type> offseter_type;
-	typedef offseter<cvector_type> coffseter_type;
-	template<typename T>
-	static bool overflow(T carry, T v1, T v2) {
-		return carry ? v1 <= v2 : v1 < v2;
-	}
-	// absolute value arith
-	template<typename T, typename U> // T, U are vector<V> or offseter<V>
-	static bool absless(const T &v1, const U &v2) {
-		if(v1.size() < v2.size()) return true;
-		if(v1.size() > v2.size()) return false;
-		return std::lexicographical_compare(v1.rbegin(), v1.rend(), v2.rbegin(), v2.rend());
-	}
-	template<typename T, typename U> // T, U are vector<V> or offseter<V>
-	static bool absgreater(const T &v1, const U &v2) {
-		if(v1.size() < v2.size()) return false;
-		if(v1.size() > v2.size()) return true;
-		return std::lexicographical_compare(v2.rbegin(), v2.rend(), v1.rbegin(), v1.rend());
-	}
-	template<typename T>
-	static std::size_t offset(const std::vector<T>&) { return 0; }
-	template<typename T> // T = vector<...>
-	static std::size_t offset(const offseter<T>& t) { return t.offset(); }
-	template<typename T, typename U> // T, U are vector<V> or offseter<V>
-	static void add(T &v1, const U &v2) {
-		if(v1.size() < v2.size()) v1.resize(v2.size());
-		value_type carry = 0;
-		for(std::size_t idx = offset(v2); idx < v1.size(); ++idx) {
-			value_type val = idx < v2.size() ? v2[idx] : 0;
-			value_type new_carry = overflow(carry,  limits_type::max() - val,  v1[idx]);
-			v1[idx] += val + carry;
-			carry = new_carry;
-			if(idx >= v2.size() && carry  == 0) break;
-		}
-		if(carry) v1.push_back(1);
-	}
-	template<typename T> // T is vector<V> or offseter<V>
-	static void normalize(T &v) {
-		auto idx = v.size() - 1; // size() should be more than 0
-		for(;v[idx] == 0 && idx > 0; --idx);
-		v.resize(idx + 1);
-	}
-	template<typename T, typename U> // T, U are vector<V> or offseter<V>
-	static bool sub(T &v1, const U &v2) {
-		bool sub = absless(v1, v2);
-		if(v1.size() < v2.size()) v1.resize(v2.size());
-		const coffseter_type &lhs = sub ? v2 : v1;
-		const coffseter_type &rhs = sub ? v1 : v2;
-		if(lhs.offset() > rhs.offset()) throw std::invalid_argument("offset mismatch");
-		// NOTE: v1.size() == lhs.size() >= rhs.size() && lhs >= rhs
-		value_type borrow = 0;
-		std::size_t idx;
-		for(idx = rhs.offset(); idx < lhs.size(); ++idx) {
-			value_type rval = idx < rhs.size() ? rhs[idx] : 0;
-			// lhs[idx] < rval + borrow
-			value_type new_borrow = overflow(borrow, lhs[idx], rval);
-			v1[idx] = lhs[idx] - rval - borrow;
-			borrow = new_borrow;
-			if( idx >= rhs.size() && borrow == 0) break;
-		}
-	 	if(borrow != 0) throw std::logic_error("Can't be borrow != 0");
-		normalize(v1);
-		return sub;
-	}
-#ifdef KARATSUBA
-	// Karatsuba algorithm
-	// At least in some environments, even for square of 10,000digits(base10) this can not outperform naive algorithm
-	static tinymp mult(const tinymp &v1, const tinymp &v2) {
-		vector_type buf(std::max<std::size_t>(700, 7*std::max(v1.v.size(), v2.v.size())));
-		auto ret = mult_imp(v1.v, v2.v, buf, 0);
-		return tinymp(ret.start(), ret.start() + ret.size()); // offset should be 0 // RVO
-	}
-	static offseter_type mult_imp(const coffseter_type &o1, const coffseter_type &o2, vector_type &buf, std::size_t cur) {
-		auto N = (std::max(o1.size(), o2.size()) + 1) / 2;
-		auto sz = 4 * N + 1;
-		offseter_type r(buf.begin() + cur, buf.begin() + cur + sz, 1, 0);
-		r[0] = 0;
-		cur += sz;
-		if(o1.size() == 0 || o2.size() == 0) {
-#if 0
-		} else if(o1.size() == 1 && o2.size() == 1) {
-			widen_type v1 = o1[0];
-			widen_type v2 = o2[0];
-			auto A = (v1 >> 16) * (v2 >> 16);
-			auto B = ((v1 & 0xFFFF) + (v1 >> 16)) * ((v2 & 0xFFFF) + (v2 >> 16));
-			auto C = (v1 & 0xFFFF) * (v2 & 0xFFFF);
-			auto ret = (A << 32) + ((B - A - C) << 16) + C;
-			r[0] = ret & 0xFFFFFFFF;
-			if(ret >> 32) r.push_back(ret >> 32);
-#endif
-		} else if(o1.size() == 1 || o2.size() == 1) {
-			widen_type carry = 0;
-			const auto &o = o2.size() == 1 ? o1 : o2;
-			auto s = o2.size() == 1 ? o2[0] : o1[0];
-			r.resize(o2.size() == 1 ? o1.size() : o2.size());
-			for(std::size_t i = 0; i < r.size(); ++i) {
-				auto temp = widen_type(o[i]) * s + carry;
-				r[i] = temp;
-				carry = temp >> limits_type::digits;
-			}
-			if(carry != 0) r.push_back(carry);
-		} else {
-			const auto mid1 = std::min(N, o1.size());
-			const auto last1 = std::min(2*N, o1.size());
-			const auto mid2 = std::min(N, o2.size());
-			const auto last2 = std::min(2*N, o2.size());
-
-			auto A = mult_imp(o1.sub_view(mid1, last1), o2.sub_view(mid2, last2), buf, cur);
-			add(r, A.offset_view(N*2));
-			sub(r, A.offset_view(N));
-
-			offseter_type a(buf.begin() + cur, buf.begin() + cur + N + 1, 1, 0);
-			a[0] = 0;
-			cur += N + 1;
-			add(a, o1.sub_view(0, mid1));
-			add(a, o1.sub_view(mid1, last1));
-			offseter_type c(buf.begin() + cur, buf.begin() + cur + N + 1, 1, 0);
-			c[0] = 0;
-			cur += N + 1;
-			add(c, o2.sub_view(0, mid2));
-			add(c, o2.sub_view(mid2, last2));
-			auto B = mult_imp(a, c, buf, cur);
-			add(r, B.offset_view(N));
-			cur -= 2 * N + 2;
-
-			auto C = mult_imp(o1.sub_view(0, mid1), o2.sub_view(0, mid2), buf, cur);
-			sub(r, C.offset_view(N));
-			add(r, C);
-		}
-//		normalize(r);
-		return r; // NRVO
-	}
-#endif
-
-	bool is_zero() const {
-		return v.size() == 1 && v[0] == 0;
-	}
-	tinymp& from_chars(const char* p, std::size_t size) {
-		if(size == 0) return *this; // might be better to throw
-		auto it = p, it_end = p + size;
-		*this = 0;
-		auto negative = false;
-		if(*it == '-') {
-			negative = true;
-			++it;
-		}
-		while(it != it_end) {
-			if('0' <= *it && *it <= '9') {
-				*this *= 10;
-				*this += (*it - '0');
-			} // otherwise, might be better to throw
-			++it;
-		}
-		if(negative) flip_();
-		return *this;
-	}
 public:
+#ifdef TINYMP_DEBUG
 	void dump(std::ostream &os) const {
 		for(auto val : v) {
 			os << val << ',';
 		}
 		os << (nonneg ? '+' : '-') << std::endl;
 	}
+#endif
 	tinymp(value_type val = 0, bool nonneg_ = true): v(1, val), nonneg(val == 0 || nonneg_ ) {}
 	tinymp(const char* p, std::size_t sz) { from_chars(p, sz); }
 	template<typename InIt>
@@ -377,7 +161,7 @@ public:
 		}
 		return p;
 	}
-#ifdef KARATSUBA
+#ifdef TINYMP_KARATSUBA
 	tinymp mult(const tinymp& other) const {
 		return mult(*this, other);
 	}
@@ -486,6 +270,231 @@ public:
 	}
 	friend inline std::ostream& operator<<(std::ostream &os, const tinymp& v) {
 		return os << to_string(v);
+	}
+
+private:
+	vector_type v;
+	bool nonneg;
+	void normalize() {
+		while(v.size() > 1 && v.back() == 0) v.pop_back();
+		if(is_zero()) nonneg = true;
+	}
+
+	template<typename T> // T = vector<...>
+	class offseter // minimal offset vector access
+	{
+	private:
+		typedef decltype(std::declval<T>().begin()) iterator_type;
+		class riterator { // lexicographical_compare() requires InputIterator
+			const offseter *p;
+			std::size_t pos; // [0, p->sz + p->offset]: (p->sz + p->off) points to p->st[p->sz - 1]
+		public:
+			typedef std::input_iterator_tag iterator_category;
+			typedef value_type value_type;
+			typedef std::ptrdiff_t difference_type;
+			typedef const value_type* pointer;
+			typedef const value_type& reference;
+			riterator(const offseter *p_, std::size_t pos_) : p(p_), pos(pos_) {}
+			reference operator*() { return pos >= p->off + 1 ? p->st[pos - p->off - 1] : zero(); }
+			riterator& operator++() { --pos; return *this; }
+			riterator operator++(int) { riterator r(*this); --pos; return r; /* NRVO */ }
+			bool operator==(const riterator& other) const { return p == other.p && pos == other.pos; }
+			bool operator!=(const riterator& other) const { return !(*this == other); }
+		};
+	public:
+		typedef typename T::value_type value_type;
+		offseter(T& pv_, std::size_t offset_ = 0) : st(pv_.begin()), sz(pv_.size()), cap(pv_.capacity()), off(offset_) {}
+		template<typename U> // should limit U
+		offseter(const offseter<U>& o) : st(o.start()), sz(o.size() - o.offset()), cap(o.capacity()), off(o.offset()) {}
+#ifdef TINYMP_KARATSUBA
+		offseter(iterator_type st_, iterator_type end_, std::size_t sz_, std::size_t offset_) : st(st_), sz(sz_), cap(end_ - st_), off(offset_) {}
+#endif
+		const value_type& operator[](std::size_t idx) const { return idx >= off ? st[idx - off] : zero(); }
+		value_type& operator[](std::size_t idx) { return idx >= off ? st[idx - off] : zero(); }
+		riterator rbegin() const { return riterator(this, sz + off); }
+		riterator rend() const { return riterator(this, 0); }
+		std::size_t size() const { return sz + off; }
+		std::size_t offset() const { return off; }
+		std::size_t capacity() const { return cap; }
+		iterator_type start() const { return st; }
+		void resize(std::size_t sz_) { if(sz_ <= cap + off && sz_ > off) { for(std::size_t idx = sz; idx < sz_ - off; ++idx) st[idx] = 0; sz = sz_ - off; } else throw std::bad_alloc(); }
+		void push_back(const value_type& t) { if(sz < cap) { st[sz] = t; ++sz; } else throw std::bad_alloc(); }
+#ifdef TINYMP_KARATSUBA
+		offseter offset_view(std::size_t o) const { return { st, st + sz, sz, off + o }; }
+		offseter sub_view(std::size_t s, std::size_t e) const { if(off != 0) throw std::logic_error("offset should be 0"); return { st + s, st + e, e - s, off }; }
+#endif
+#ifdef TINYMP_DEBUG
+		void dump(std::ostream &os) const {
+			os << "[sz:" << sz << ",cap:" << cap << ",off:" << off << ']';
+			for(std::size_t idx = 0; idx < sz; ++idx) os << ',' << st[idx];
+			os << std::endl;
+		}
+#endif
+	private:
+		iterator_type st;
+		std::size_t sz; // valid digits
+		std::size_t cap;
+		std::size_t off;
+		static value_type& zero() {
+			static value_type zero_;
+			return zero_;
+		}
+	};
+	typedef offseter<vector_type> offseter_type;
+	typedef offseter<cvector_type> coffseter_type;
+	template<typename T>
+	static bool overflow(T carry, T v1, T v2) {
+		return carry ? v1 <= v2 : v1 < v2;
+	}
+	// absolute value arith
+	template<typename T, typename U> // T, U are vector<V> or offseter<V>
+	static bool absless(const T &v1, const U &v2) {
+		if(v1.size() < v2.size()) return true;
+		if(v1.size() > v2.size()) return false;
+		return std::lexicographical_compare(v1.rbegin(), v1.rend(), v2.rbegin(), v2.rend());
+	}
+	template<typename T, typename U> // T, U are vector<V> or offseter<V>
+	static bool absgreater(const T &v1, const U &v2) {
+		if(v1.size() < v2.size()) return false;
+		if(v1.size() > v2.size()) return true;
+		return std::lexicographical_compare(v2.rbegin(), v2.rend(), v1.rbegin(), v1.rend());
+	}
+	template<typename T>
+	static std::size_t offset(const std::vector<T>&) { return 0; }
+	template<typename T> // T = vector<...>
+	static std::size_t offset(const offseter<T>& t) { return t.offset(); }
+	template<typename T, typename U> // T, U are vector<V> or offseter<V>
+	static void add(T &v1, const U &v2) {
+		if(v1.size() < v2.size()) v1.resize(v2.size());
+		value_type carry = 0;
+		for(std::size_t idx = offset(v2); idx < v1.size(); ++idx) {
+			value_type val = idx < v2.size() ? v2[idx] : 0;
+			value_type new_carry = overflow(carry,  limits_type::max() - val,  v1[idx]);
+			v1[idx] += val + carry;
+			carry = new_carry;
+			if(idx >= v2.size() && carry  == 0) break;
+		}
+		if(carry) v1.push_back(1);
+	}
+	template<typename T> // T is vector<V> or offseter<V>
+	static void normalize(T &v) {
+		auto idx = v.size() - 1; // size() should be more than 0
+		for(;v[idx] == 0 && idx > 0; --idx);
+		v.resize(idx + 1);
+	}
+	template<typename T, typename U> // T, U are vector<V> or offseter<V>
+	static bool sub(T &v1, const U &v2) {
+		bool sub = absless(v1, v2);
+		if(v1.size() < v2.size()) v1.resize(v2.size());
+		const coffseter_type &lhs = sub ? v2 : v1;
+		const coffseter_type &rhs = sub ? v1 : v2;
+		if(lhs.offset() > rhs.offset()) throw std::invalid_argument("offset mismatch");
+		// NOTE: v1.size() == lhs.size() >= rhs.size() && lhs >= rhs
+		value_type borrow = 0;
+		std::size_t idx;
+		for(idx = rhs.offset(); idx < lhs.size(); ++idx) {
+			value_type rval = idx < rhs.size() ? rhs[idx] : 0;
+			// lhs[idx] < rval + borrow
+			value_type new_borrow = overflow(borrow, lhs[idx], rval);
+			v1[idx] = lhs[idx] - rval - borrow;
+			borrow = new_borrow;
+			if( idx >= rhs.size() && borrow == 0) break;
+		}
+	 	if(borrow != 0) throw std::logic_error("Can't be borrow != 0");
+		normalize(v1);
+		return sub;
+	}
+#ifdef TINYMP_KARATSUBA
+	// Karatsuba algorithm
+	// At least in some environments, even for square of 10,000digits(base10) this can not outperform naive algorithm
+	static tinymp mult(const tinymp &v1, const tinymp &v2) {
+		vector_type buf(std::max<std::size_t>(700, 7*std::max(v1.v.size(), v2.v.size())));
+		auto ret = mult_imp(v1.v, v2.v, buf, 0);
+		return tinymp(ret.start(), ret.start() + ret.size()); // offset should be 0 // RVO
+	}
+	static offseter_type mult_imp(const coffseter_type &o1, const coffseter_type &o2, vector_type &buf, std::size_t cur) {
+		auto N = (std::max(o1.size(), o2.size()) + 1) / 2;
+		auto sz = 4 * N + 1;
+		offseter_type r(buf.begin() + cur, buf.begin() + cur + sz, 1, 0);
+		r[0] = 0;
+		cur += sz;
+		if(o1.size() == 0 || o2.size() == 0) {
+#if 0
+		} else if(o1.size() == 1 && o2.size() == 1) {
+			widen_type v1 = o1[0];
+			widen_type v2 = o2[0];
+			auto A = (v1 >> 16) * (v2 >> 16);
+			auto B = ((v1 & 0xFFFF) + (v1 >> 16)) * ((v2 & 0xFFFF) + (v2 >> 16));
+			auto C = (v1 & 0xFFFF) * (v2 & 0xFFFF);
+			auto ret = (A << 32) + ((B - A - C) << 16) + C;
+			r[0] = ret & 0xFFFFFFFF;
+			if(ret >> 32) r.push_back(ret >> 32);
+#endif
+		} else if(o1.size() == 1 || o2.size() == 1) {
+			widen_type carry = 0;
+			const auto &o = o2.size() == 1 ? o1 : o2;
+			auto s = o2.size() == 1 ? o2[0] : o1[0];
+			r.resize(o2.size() == 1 ? o1.size() : o2.size());
+			for(std::size_t i = 0; i < r.size(); ++i) {
+				auto temp = widen_type(o[i]) * s + carry;
+				r[i] = temp;
+				carry = temp >> limits_type::digits;
+			}
+			if(carry != 0) r.push_back(carry);
+		} else {
+			const auto mid1 = std::min(N, o1.size());
+			const auto last1 = std::min(2*N, o1.size());
+			const auto mid2 = std::min(N, o2.size());
+			const auto last2 = std::min(2*N, o2.size());
+
+			auto A = mult_imp(o1.sub_view(mid1, last1), o2.sub_view(mid2, last2), buf, cur);
+			add(r, A.offset_view(N*2));
+			sub(r, A.offset_view(N));
+
+			offseter_type a(buf.begin() + cur, buf.begin() + cur + N + 1, 1, 0);
+			a[0] = 0;
+			cur += N + 1;
+			add(a, o1.sub_view(0, mid1));
+			add(a, o1.sub_view(mid1, last1));
+			offseter_type c(buf.begin() + cur, buf.begin() + cur + N + 1, 1, 0);
+			c[0] = 0;
+			cur += N + 1;
+			add(c, o2.sub_view(0, mid2));
+			add(c, o2.sub_view(mid2, last2));
+			auto B = mult_imp(a, c, buf, cur);
+			add(r, B.offset_view(N));
+			cur -= 2 * N + 2;
+
+			auto C = mult_imp(o1.sub_view(0, mid1), o2.sub_view(0, mid2), buf, cur);
+			sub(r, C.offset_view(N));
+			add(r, C);
+		}
+//		normalize(r);
+		return r; // NRVO
+	}
+#endif
+
+	bool is_zero() const {
+		return v.size() == 1 && v[0] == 0;
+	}
+	tinymp& from_chars(const char* p, std::size_t size) {
+		if(size == 0) return *this; // might be better to throw
+		auto it = p, it_end = p + size;
+		*this = 0;
+		auto negative = false;
+		if(*it == '-') {
+			negative = true;
+			++it;
+		}
+		while(it != it_end) {
+			if('0' <= *it && *it <= '9') {
+				*this *= 10;
+				*this += (*it - '0');
+			} // otherwise, might be better to throw
+			++it;
+		}
+		if(negative) flip_();
+		return *this;
 	}
 };
 template<char ... c>
