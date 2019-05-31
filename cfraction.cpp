31782f6c1e32623e696c450b65a23c7c207b4428
Yak! <yak1ex@users.noreply.github.com>
2019-05-31 21:02:53 +0900

    Add approximation functions (see GH-2).

--- cfraction.cpp
+++ cfraction.cpp
@@ -46,13 +46,13 @@ struct cfraction
 		swap(n, d);
 		return { n, d };
 	}
-	static cfraction fit(const cfraction &v1, const cfraction &v2) { // FIXME: dumb logic
+	static cfraction best(const cfraction &v1, const cfraction &v2) { // FIXME: dumb logic
 		auto r1 = rational<T>(v1);
 		auto r2 = rational<T>(v2);
 		if(r1 == r2) throw std::domain_error("requires non-empty interval");
-		auto r = fit_imp(v1, v2); auto rr = rational<T>(r);
+		auto r = best_imp(v1, v2); auto rr = rational<T>(r);
 		auto update = [&](const cfraction &vv1, const cfraction &vv2){ 
-			auto r_ = fit_imp(vv1, vv2); auto rr_ = rational<T>(r_);
+			auto r_ = best_imp(vv1, vv2); auto rr_ = rational<T>(r_);
 			if(rr_ != r1 && rr_ != r2  && (rr == r1 || rr == r2 || rr.den() > rr_.den())) { r = r_; rr = rr_; }
 		};
 		update(v1, v2.alt());
@@ -60,7 +60,28 @@ struct cfraction
 		update(v1.alt(), v2.alt());
 		return r;
 	}
-	cfraction fit(const cfraction &v) const { return fit(*this, v); }
+	cfraction best(const cfraction &v) const { return best(*this, v); }
+	template<typename F>
+	void foreach_approx(F f) const {
+		cfraction app(v.begin(), v.begin()+1);
+		for(std::size_t idx = 1; idx < v.size(); ++idx) {
+			app.v.push_back(0);
+			for(T rval = v[idx] / 2 + (v[idx] % 2 == 1 || !check_half(idx)); rval <= v[idx]; ++rval) {
+				app.v[idx] = rval;
+				if(f(app)) break;
+			}
+		}
+	}
+	rational<T> approx(T denom) const {
+		rational<T> r;
+		foreach_approx([&r,denom](const cfraction &val){
+			auto t = rational<T>(val);
+			if(t.den() > denom) return true;
+			r = std::move(t);
+			return false;
+		});
+		return r;
+	}
 	friend std::ostream& operator<<(std::ostream& os, const cfraction &v) {
 		int n = 0;
 		os << '[';
@@ -73,13 +94,18 @@ struct cfraction
 	}
 private:
 	std::size_t min_size() const { return v.size() - (v.size() != 1 &&  v.back() == 1); }
-	static cfraction fit_imp(const cfraction &v1, const cfraction &v2) {
+	static cfraction best_imp(const cfraction &v1, const cfraction &v2) {
 		auto N = std::min(v1.v.size(), v2.v.size());
 		auto e = std::mismatch(v1.v.begin(), v1.v.begin() + N, v2.v.begin());
 		cfraction ret(0);
 		ret.v.assign(v1.v.begin(), e.first);
 		ret.v.push_back((e.first == v1.v.end() ? *e.second : e.second == v2.v.end() ? *e.first : std::min(*e.first, *e.second)) + 1);
 		return ret;
+	}
+	bool check_half(std::size_t idx) const {
+		cfraction v1(v.rend() - idx - 1, v.rend() - 1);
+		cfraction v2(v.begin() + idx, v.end());
+		return rational<T>(v1) > rational<T>(v2);
 	}
 	std::vector<T> v;
 };
--- cfraction_test.cpp
+++ cfraction_test.cpp
@@ -7,8 +7,29 @@ BOOST_AUTO_TEST_CASE( cfraction_test )
 	BOOST_TEST( cfraction<int>(rational<int>(13, 5)).alt() == cfraction<int>({2, 1, 1, 1, 1}) );
 	BOOST_TEST( cfraction<int>(rational<int>(13, 5)).alt().alt() == cfraction<int>({2, 1, 1, 2}) );
 	BOOST_TEST( rational<int>(cfraction<int>(rational<int>(13, 5))) == rational<int>(13, 5) );
-	BOOST_TEST( cfraction<int>(rational<int>(13, 100)).fit(rational<int>(14, 100)) == cfraction<int>({0, 7, 2}) );
-	BOOST_TEST( rational<int>(cfraction<int>(rational<int>(13, 100)).fit(rational<int>(14, 100))) == rational<int>(2, 15) );
-	BOOST_TEST( cfraction<int>(rational<int>(1, 2)).fit(rational<int>(1, 3)) == cfraction<int>({0, 2, 2}) );
-	BOOST_TEST( rational<int>(cfraction<int>(rational<int>(1, 2)).fit(rational<int>(1, 3))) == rational<int>(2, 5) );
+	BOOST_TEST( cfraction<int>(rational<int>(13, 100)).best(rational<int>(14, 100)) == cfraction<int>({0, 7, 2}) );
+	BOOST_TEST( rational<int>(cfraction<int>(rational<int>(13, 100)).best(rational<int>(14, 100))) == rational<int>(2, 15) );
+	BOOST_TEST( cfraction<int>(rational<int>(1, 2)).best(rational<int>(1, 3)) == cfraction<int>({0, 2, 2}) );
+	BOOST_TEST( rational<int>(cfraction<int>(rational<int>(1, 2)).best(rational<int>(1, 3))) == rational<int>(2, 5) );
+	// example from https://en.wikipedia.org/wiki/Continued_fraction#Best_rational_approximations
+	std::vector<cfraction<int>> v, vr = {
+		{ 0, 1 },
+		{ 0, 1, 3 },
+		{ 0, 1, 4 },
+		{ 0, 1, 5 },
+		{ 0, 1, 5, 2 },
+		{ 0, 1, 5, 2, 1 },
+		{ 0, 1, 5, 2, 2 }
+	};
+	cfraction<int>(rational<int>(84375, 100000)).foreach_approx([&v](const cfraction<int> &p){
+		v.push_back(p); return false;
+	});
+	BOOST_TEST( cfraction<int>(rational<int>(84375, 100000)).approx(4) == rational<int>(3, 4) );
+	BOOST_TEST( cfraction<int>(rational<int>(84375, 100000)).approx(5) == rational<int>(4, 5) );
+	BOOST_TEST( cfraction<int>(rational<int>(84375, 100000)).approx(6) == rational<int>(5, 6) );
+	BOOST_TEST( cfraction<int>(rational<int>(84375, 100000)).approx(13) == rational<int>(11, 13) );
+	BOOST_TEST( cfraction<int>(rational<int>(84375, 100000)).approx(19) == rational<int>(16, 19) );
+	BOOST_TEST( cfraction<int>(rational<int>(84375, 100000)).approx(32) == rational<int>(27, 32) );
+
+	BOOST_TEST( cfraction<int>(rational<int>(314159, 100000)).approx(1000) == rational<int>(355, 113) );
 }
